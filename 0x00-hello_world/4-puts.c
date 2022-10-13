540ad71e77eba388dbbfecd1e4aab74f8839c803
shalom90 <ibukoye92@gmail.com>
2022-10-13 13:15:47 -0700

    Task 4- Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main- Execution starts and ends here
+  * Return: Returns 0 if no error is found, else return non-zero value
+  */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
