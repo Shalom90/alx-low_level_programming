5976b3b9330fc90ee7a7b578ea087ee102bda048
shalom90 <ibukoye92@gmail.com>
2022-10-13 13:38:29 -0700

    Task 4- Hello puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * stdio.h - Header file for library functions
+  * main - Execution starts and end here
+  * Return: Returns 0 if no error occurs else return a non-zero value
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
