3f5d1f5af9255cf8e4f17a94a9cb7d43af5a92a4
shalom90 <ibukoye92@gmail.com>
2022-10-13 15:11:01 -0700

    Task 8- UNIX is basically a simple operating system but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+  * main- Execution starts and ends here
+  * Return: returns 0 if program runs else return 1 if any error
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
