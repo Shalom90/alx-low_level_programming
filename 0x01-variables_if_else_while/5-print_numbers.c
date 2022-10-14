a9161f8fb69d35bcdf49e54037b90d08609e01c2
shalom90 <ibukoye92@gmail.com>
2022-10-14 11:42:20 -0700

    Task 5- Print single digit numbers of base 10 from Zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main- prints all single digit numbers of base 10 from 0
+  * Return: returns 0 if program runs successfully
+  */
+
+int main(void)
+{
+
+	int x = '0';
+
+	for (x = 0; x < 10; x++)
+
+	printf("%d\n", x);
+	return (0);
+
+}
