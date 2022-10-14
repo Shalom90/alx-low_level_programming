783acf9a4bc378301f64e2b43b3a18e998c1934a
shalom90 <ibukoye92@gmail.com>
2022-10-14 13:08:48 -0700

    Task 9- Print all possible combinations of single-digit numbers with comma and space between them using putchar function

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main- program prints all possible combimaions of single digit numbers
+  * using putchar function only
+  * Return: 0 returns if program is successful
+  */
+
+int main(void)
+{
+	int myNum = 0;
+
+	for (myNum = 0; myNum < 10; myNum++)
+
+	{
+		putchar((myNum % 10) + '0');
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
