11206dcec0824c94df9bbb878fd510bbcc984957
shalom90 <ibukoye92@gmail.com>
2022-10-18 17:17:27 -0700

    Task 8- To write a function which prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,33 +4,19 @@
   * Return: Always 0
   */
 
-void jack_bauer(void)
+void  jack_bauer(void)
 {
-	int a, b, c, d;
-
-	for (a = 0; a <= 2; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			for (c = 0; c <= 9; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a == 2 && b > 3)
-					{
-						break;
-					}
-					{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-					}
-				}
-			}
-		}
-	}
-
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
+{
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar('0' + (hour / 10));
+_putchar('0' + (hour % 10));
+_putchar(':');
+_putchar('0' + (minute / 10));
+_putchar('0' + (minute % 10));
+_putchar('\n');
+}
+}
 }
