4ad83e001bad580afe0e88b7b1cedb6b3d043c8d
shalom90 <ibukoye92@gmail.com>
2022-10-20 11:31:15 -0700

    Task 6- To write a function which prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,23 +6,23 @@
 
 void print_square(int size)
 {
-	if (size > 0)
+	if (size <= 0)
 	{
-		int a;
-
-		for (a = 1; a <= size, a++)
-		{
-			int b;
-
-			for (b = 1; b <= size; b++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n);
-		}
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		int a, b;
+
+		for (a = 0; a < size; a++)
+
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
