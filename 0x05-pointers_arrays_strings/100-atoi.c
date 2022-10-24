bc15624691c0c38b806f5cbc5fca83ab05615192
shalom90 <ibukoye92@gmail.com>
2022-10-24 13:07:10 -0700

    Task 10 - Write a function which converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * _atoi- converts a string to an integer
+  * @s: string
+  * Return: the int converted from the string
+  */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int dig = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+
+			dig = (dig * 10) + (*s - '0');
+
+		else if (dig > 0)
+			break;
+	} while (*s++)
+
+	return (dig * sign);
+
+
+}
