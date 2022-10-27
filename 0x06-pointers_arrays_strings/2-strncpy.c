aec9bfdcb4fceaa991656134a9e0a01a78ef6b90
shalom90 <ibukoye92@gmail.com>
2022-10-27 09:00:43 -0700

    Task 2- To write a function which copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * *_strncpy- copies a string
+  * @dest: destinaion
+  * @src: source
+  * @n: number of bytes from source
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+
+		dest[a] = src[a];
+
+	for ( ; a < n; a++)
+
+		dest[a] = '\0';
+
+	return (dest);
+}
