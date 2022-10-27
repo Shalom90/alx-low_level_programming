44ec028e751706e9b98f88face8ab3aa40ecc5a7
shalom90 <ibukoye92@gmail.com>
2022-10-27 13:41:10 -0700

    Advanced Task 1- To write a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * rot13- encodes a string in rot13
+  * @s: string to be encoded
+  * Return: the pointer to dest.
+  */
+
+char *rot13(char *s)
+{
+	int x, y;
+
+	char *i = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *j = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; i[y] != '\0'\; y++)
+		{
+			if (s[x] = i[y])
+			{
+				s[x] = j[y];
+
+				break;
+			}
+		}
+	}
+
+	return (s);
+
+}
